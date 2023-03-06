f33cbb3b9646f7101ffad88c39ebe2a9329a33d4
astonlyj <115874272+astonlyj@users.noreply.github.com>
2023-03-06 16:45:52 +0800

    Add Recipe Detail API

--- app/user/tests/test_user_api.py
+++ app/user/tests/test_user_api.py
@@ -115,9 +115,9 @@ class PrivateUserAPITests(TestCase):
 
     def setUp(self):
         self.user = create_user(
-            email = 'test@example.com',
-            password = 'testpass123',
-            name = 'Test Name',
+            email='test@example.com',
+            password='testpass123',
+            name='Test Name',
         )
         self.client = APIClient()
         self.client.force_authenticate(user=self.user)
